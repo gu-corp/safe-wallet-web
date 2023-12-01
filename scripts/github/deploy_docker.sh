5148e435a847111b4c81ea89aa8feb6afd71f5c0
cuonghx-gutech <cuonghx@gu-tech.com>
2023-12-01 11:15:35 +0700

    Using gulabs namespace

--- scripts/github/deploy_docker.sh
+++ scripts/github/deploy_docker.sh
@@ -6,19 +6,19 @@ export DOCKER_BUILDKIT=1
 
 if [ "$1" = "dev" -o "$1" = "main" ]; then
     # If image does not exist, don't use cache
-    docker pull cuonghx/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT . --cache-from cuonghx/$DOCKERHUB_PROJECT:$1 --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker pull gulabs/$DOCKERHUB_PROJECT:$1 && \
+    docker build -t $DOCKERHUB_PROJECT . --cache-from gulabs/$DOCKERHUB_PROJECT:$1 --build-arg BUILDKIT_INLINE_CACHE=1 || \
     docker build -t $DOCKERHUB_PROJECT . --build-arg BUILDKIT_INLINE_CACHE=1
 else
     # Building tag version from staging image (vX.X.X)
-    docker pull cuonghx/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT . --cache-from cuonghx/$DOCKERHUB_PROJECT:staging --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker pull gulabs/$DOCKERHUB_PROJECT:staging && \
+    docker build -t $DOCKERHUB_PROJECT . --cache-from gulabs/$DOCKERHUB_PROJECT:staging --build-arg BUILDKIT_INLINE_CACHE=1 || \
     docker build -t $DOCKERHUB_PROJECT . --build-arg BUILDKIT_INLINE_CACHE=1
     # Only push latest on release
     case $1 in v*)
-        docker tag $DOCKERHUB_PROJECT cuonghx/$DOCKERHUB_PROJECT:latest
-        docker push cuonghx/$DOCKERHUB_PROJECT:latest
+        docker tag $DOCKERHUB_PROJECT gulabs/$DOCKERHUB_PROJECT:latest
+        docker push gulabs/$DOCKERHUB_PROJECT:latest
     esac
 fi
-docker tag $DOCKERHUB_PROJECT cuonghx/$DOCKERHUB_PROJECT:$1
-docker push cuonghx/$DOCKERHUB_PROJECT:$1
+docker tag $DOCKERHUB_PROJECT gulabs/$DOCKERHUB_PROJECT:$1
+docker push gulabs/$DOCKERHUB_PROJECT:$1
